f63fa8d448754514175573f5769f5466978d017f
allrexup21 <mugerererahman@gmail.com>
2024-09-22 11:58:18 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double sum = 0.0;
+
+    for (int numerator = 1, denominator = 3; numerator <= 95; numerator += 2, denominator += 2) {
+        sum += static_cast<double>(numerator) / denominator;
+    }
+    cout << "The sum of the series is: " << sum << endl;
+
+    return 0;
+}
